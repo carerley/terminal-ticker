771d01594a6b4c6f1b6952f0172a272f52e0c957
Alex Li <carerley@gmail.com>
2026-08-16 19:26:01 -0400

    Update Homebrew reference for v0.3.0

--- packaging/homebrew/ticker.rb
+++ packaging/homebrew/ticker.rb
@@ -1,8 +1,8 @@
 class Ticker < Formula
   desc "Quick no-key stock quotes for the terminal"
   homepage "https://github.com/carerley/terminal-ticker"
-  url "https://github.com/carerley/terminal-ticker/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "c73bc7cd35f896520af33052cdff793a6ddf70db64ec9115423efcf45e98afd9"
+  url "https://github.com/carerley/terminal-ticker/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "e22ba05323c1d7b22de046d1beb68f993939ee28ace75cf84c97f7e10fdf2643"
   license "MIT"
 
   depends_on "python"
@@ -21,6 +21,6 @@ class Ticker < Formula
   end
 
   test do
-    assert_match "ticker 0.2.0", shell_output("#{bin}/ticker --version")
+    assert_match "ticker 0.3.0", shell_output("#{bin}/ticker --version")
   end
 end
